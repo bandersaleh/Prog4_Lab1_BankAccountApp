3f8af741cfb021e15662069e42255b1d285acdbb
Bander Saleh <bandersaleh93@gmail.com>
2023-05-23 16:03:22 -0700

    Added required code in the Account Class and it's really finalized looking. Next missing is child classes code.

--- BankAccountConsoleApp/BankAccountConsoleApp/Account.cs
+++ BankAccountConsoleApp/BankAccountConsoleApp/Account.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BankAccountConsoleApp
 {
-    internal class Account
+    // Abstract keyword is a class and method modifier
+    public abstract class Account
     {
+        // Field
+        double _balance;
+
+        // Constructor
+        protected Account(double balance)
+        {
+            _balance = balance;
+        }
+
+        // Methods
+        // Property (for _balance) allows the children to set the balance, but restricts other classes
+        public double Balance { get => _balance; protected set => _balance = value; } // => is the symbol for Lambda. It's good for writing a quick method that we can delete later that isn't as permanent as method. Lambdas allows us to write an in-line/custom method as seen in our Properties! To use Lambdas we have to use a Delegate. It is used to make code more efficient (Takes up less space than a method, etc...) It's similiar to writing a Function in Javascript.
+
+        protected abstract void Deposit(double);
+        //protected abstract bool Withdraw(double);
+
     }
+
 }
