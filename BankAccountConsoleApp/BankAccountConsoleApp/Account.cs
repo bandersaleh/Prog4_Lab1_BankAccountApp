2ca614ca72002faad5c3645a220bb8cfc64c455f
Bander Saleh <bandersaleh93@gmail.com>
2023-05-26 22:06:30 -0700

    Commented out 1 line of code from almost finalized Account.cs parent class

--- BankAccountConsoleApp/BankAccountConsoleApp/Account.cs
+++ BankAccountConsoleApp/BankAccountConsoleApp/Account.cs
@@ -24,7 +24,7 @@ namespace BankAccountConsoleApp
         // Property (for _balance) allows the children to set the balance, but restricts other classes
         public double Balance { get => _balance; protected set => _balance = value; } // => is the symbol for Lambda. It's good for writing a quick method that we can delete later that isn't as permanent as method. Lambdas allows us to write an in-line/custom method as seen in our Properties! To use Lambdas we have to use a Delegate. It is used to make code more efficient (Takes up less space than a method, etc...) It's similiar to writing a Function in Javascript.
 
-        protected abstract void Deposit(double);
+        //protected abstract void Deposit(double);
         //protected abstract bool Withdraw(double);
 
     }
